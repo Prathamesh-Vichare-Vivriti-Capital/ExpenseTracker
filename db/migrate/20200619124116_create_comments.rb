7099f25769fe3deb0a6b5ebe305a8772ecb7737d
Prathamesh-Vichare-Vivriti-Capital <prathamesh.vichare@vivriticapital.com>
2020-07-03 14:42:38 +0530

    Error in previous migration create_comment drop table

--- db/migrate/20200619124116_create_comments.rb
+++ db/migrate/20200619124116_create_comments.rb
@@ -1,6 +1,5 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
-    drop_table :comments
     create_table :comments do |t|
       t.string :commentable_type
       t.integer :commentable_id
